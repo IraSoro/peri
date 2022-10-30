a028a17f3271475d76507c24589726e66d256613
Irina Sorokina <irasoro402@yandex.ru>
2022-10-30 14:53:25 +0400

    Added average info

--- src/pages/TabHistory.tsx
+++ src/pages/TabHistory.tsx
@@ -1,6 +1,7 @@
 import {
   IonContent,
   IonPage,
+  IonLabel,
 } from '@ionic/react';
 import './TabHistory.css';
 
@@ -9,7 +10,14 @@ const TabHistory: React.FC = () => {
     <IonPage>
       <IonContent color="light" fullscreen>
         <div id="rectangle"></div>
-        <div id="circle"></div>
+        <div id="circle">
+          <IonLabel >
+            <p style={{ fontSize: "10px", color: "rgb(var(--ion-color-basic-rgb))", textAlign: "center" }}>Period length</p>
+            <h1 style={{ fontWeight: "bold", color: "rgb(var(--ion-color-dark-basic-rgb))", textAlign: "center" }}>5 Days</h1>
+            <p style={{ fontSize: "10px", color: "rgb(var(--ion-color-basic-rgb))", textAlign: "center" }}>Cycle length</p>
+            <h1 style={{ fontWeight: "bold", color: "rgb(var(--ion-color-dark-basic-rgb))", textAlign: "center" }}>25 Days</h1>
+          </IonLabel>
+        </div>
         <div id="rectangle-angle"></div>
       </IonContent>
     </IonPage>
