36bc8e70b043ca41d9287ab9fdac574bc48a4274
Irina Sorokina <irasoro402@yandex.ru>
2022-10-31 21:42:40 +0400

    Changed shapes drawing

--- src/pages/TabHistory.tsx
+++ src/pages/TabHistory.tsx
@@ -11,8 +11,8 @@ const TabHistory: React.FC = () => {
 
   return (
     <IonPage>
-      <IonContent color="light" fullscreen>
-        <div id="rectangle">
+      <IonContent color="basic" fullscreen>
+        <div id="rectangle-top">
           <div id="circle">
             <IonLabel >
               <p style={{ fontSize: "10px", color: "rgb(var(--ion-color-basic-rgb))", textAlign: "center" }}>Period length</p>
@@ -22,15 +22,16 @@ const TabHistory: React.FC = () => {
             </IonLabel>
           </div>
         </div>
-        {/* <div id="rectangle-angle"></div> */}
-        <IonLabel style={{ fontSize: "12px" }}>12 Seb - Current</IonLabel>
-        <IonProgressBar value={.05 * 3} buffer={.25 * 3}></IonProgressBar>
+        <div id="rectangle-bottom">
+          <IonLabel style={{ fontSize: "12px" }}>12 Seb - Current</IonLabel>
+          <IonProgressBar value={.05 * 3} buffer={.25 * 3}></IonProgressBar>
 
-        <IonLabel style={{ fontSize: "12px" }}>12 Aug - 1 Seb</IonLabel>
-        <IonProgressBar value={.05 * 3} buffer={.26 * 3}></IonProgressBar>
+          <IonLabel style={{ fontSize: "12px" }}>12 Aug - 1 Seb</IonLabel>
+          <IonProgressBar value={.05 * 3} buffer={.26 * 3}></IonProgressBar>
 
-        <IonLabel style={{ fontSize: "12px" }}>12 July - 1 Aug</IonLabel>
-        <IonProgressBar value={.04 * 3} buffer={.23 * 3}></IonProgressBar>
+          <IonLabel style={{ fontSize: "12px" }}>12 July - 1 Aug</IonLabel>
+          <IonProgressBar value={.04 * 3} buffer={.23 * 3}></IonProgressBar>
+        </div>
       </IonContent>
     </IonPage>
   );
