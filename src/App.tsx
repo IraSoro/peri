9e4b52c7d9f20832077b0046a7dcf7b1d8b7bee4
Irina Sorokina <irasoro402@yandex.ru>
2022-11-18 14:31:12 +0400

    Removed swiper

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import {
 import { IonReactRouter } from '@ionic/react-router';
 import TabHome from './pages/TabHome';
 import TabHistory from './pages/TabHistory';
-import SwiperStart from './pages/Swiper';
 import './App.css';
 
 /* Core CSS required for Ionic components to work properly */
@@ -55,7 +54,6 @@ const App: React.FC = () => {
 
   return (
     <IonApp>
-      <SwiperStart />
       <IonReactRouter>
         <IonHeader class="ion-no-border">
           <IonToolbar color="basic">
@@ -72,11 +70,8 @@ const App: React.FC = () => {
               <Route exact path="/details">
                 <TabHistory />
               </Route>
-              <Route path="/swiper">
-                <SwiperStart />
-              </Route>
               <Route exact path="/">
-                <Redirect to="/swiper" />
+                <Redirect to="/home" />
               </Route>
             </IonRouterOutlet>
 
