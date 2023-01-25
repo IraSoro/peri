401b0550be85632f77f32094cdc987f8196d4757
Irina Sorokina <irasoro402@yandex.ru>
2023-01-26 00:25:10 +0400

    Update README

--- src/pages/TabHome.tsx
+++ src/pages/TabHome.tsx
@@ -22,7 +22,7 @@ import InfoModal from './InfoModal';
 
 import uterus from '../assets/uterus.svg';
 
-import { get } from '../data/Storage';
+import { get, clear } from '../data/Storage';
 import {
   getInfo,
   InfoCurrentCycle,
@@ -51,9 +51,10 @@ const TabHome = (props: MainProps) => {
 
   useEffect(() => {
     get("welcome").then(result => {
-      if (!result) {
+      // if (!result) {
+        clear();
         setIsWelcomeModal(true);
-      }
+      // }
     });
 
     get("current-cycle").then(resultData => {
