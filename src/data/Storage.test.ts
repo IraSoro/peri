c21678e02018b04f97dfbe35002f44a4afdd8457
Yury Getman <imblowfish@duck.com>
2023-06-30 01:04:47 +0400

    Added `Storage` tests

---
+++ src/data/Storage.test.ts
@@ -0,0 +1,52 @@
+// import * as mockedIonicStorage from "@ionic/storage";
+import { Storage } from "@ionic/storage";
+
+import { storage } from "./Storage";
+
+test("setCycles", async () => {
+  const storageSetSpy = jest
+    .spyOn(Storage.prototype, "set")
+    .mockResolvedValueOnce({});
+
+  const cycles = [
+    {
+      cycleLength: 28,
+      periodLength: 6,
+      startDate: "2023-05-31",
+    },
+  ];
+
+  await storage.set.cycles(cycles);
+
+  expect(storageSetSpy).toHaveBeenCalledWith("cycles", cycles);
+});
+
+describe("getCycles", () => {
+  test("There are no cycles in storage", async () => {
+    const storageGetSpy = jest
+      .spyOn(Storage.prototype, "get")
+      .mockResolvedValueOnce(undefined);
+
+    await expect(storage.get.cycles()).rejects.toThrow(
+      "Can't find `cycles` in storage"
+    );
+    expect(storageGetSpy).toHaveBeenCalledWith("cycles");
+  });
+
+  test("Storage has cycles", async () => {
+    const cycles = [
+      {
+        cycleLength: 28,
+        periodLength: 6,
+        startDate: "2023-05-31",
+      },
+    ];
+
+    const storageGetSpy = jest
+      .spyOn(Storage.prototype, "get")
+      .mockResolvedValueOnce(cycles);
+
+    await expect(storage.get.cycles()).resolves.toBe(cycles);
+    expect(storageGetSpy).toHaveBeenCalledWith("cycles");
+  });
+});
