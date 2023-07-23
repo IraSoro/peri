e73af92a749b6456be2fa41857823518deac1f62
Irina Sorokina <irasoro402@yandex.ru>
2023-07-24 01:33:43 +0400

    Fixed i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-// import LanguageDetector from 'i18next-browser-languagedetector';
+// import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "./translations/en";
 import ru from "./translations/ru";
@@ -15,12 +15,12 @@ const resources = {
 };
 
 i18n
-  //   .use(LanguageDetector)
+  // .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
     lng: "en",
-    fallbackLng: "ru",
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false,
     },
