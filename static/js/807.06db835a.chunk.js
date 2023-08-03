7597778c3f0ace1852e961c23824dd2d39190be2
Irina Sorokina <irasoro402@yandex.ru>
2023-08-04 00:41:23 +0400

    Updates

---
+++ static/js/807.06db835a.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 807.06db835a.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkperi=self.webpackChunkperi||[]).push([[807],{807:function(e,n,r){r.r(n),r.d(n,{g:function(){return u}});var u=r(5670).i}}]);
+//# sourceMappingURL=807.06db835a.chunk.js.map
--- static/js/807.9a02777f.chunk.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 807.9a02777f.chunk.js.LICENSE.txt */
-"use strict";(self.webpackChunkperi=self.webpackChunkperi||[]).push([[807],{807:function(e,n,r){r.r(n),r.d(n,{g:function(){return u}});var u=r(3878).i}}]);
-//# sourceMappingURL=807.9a02777f.chunk.js.map
