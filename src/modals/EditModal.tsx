97a9226934f50d92a26c52d73489652f4f20ad07
Irina Sorokina <irasoro402@yandex.ru>
2023-08-21 15:46:52 +0400

    Added calculations for editable cycles

--- src/modals/EditModal.tsx
+++ src/modals/EditModal.tsx
@@ -10,6 +10,7 @@ import { useTranslation } from "react-i18next";
 import { format } from "date-fns";
 import "./EditModal.css";
 
+import type { Cycle } from "../data/ClassCycle";
 import { CyclesContext } from "../state/Context";
 
 interface PropsEditModal {
@@ -21,7 +22,7 @@ const EditModal = (props: PropsEditModal) => {
   const datetimeRef = useRef<null | HTMLIonDatetimeElement>(null);
   const { t } = useTranslation();
 
-  const { cycles } = useContext(CyclesContext);
+  const { cycles, updateCycles } = useContext(CyclesContext);
 
   const nowDate = new Date();
   nowDate.setHours(0, 0, 0, 0);
@@ -46,7 +47,9 @@ const EditModal = (props: PropsEditModal) => {
     for (const cycle of cycles) {
       const startOfCycle = new Date(cycle.startDate);
       startOfCycle.setHours(0, 0, 0, 0);
-      for (let i = 0; i < cycle.periodLength; i++) {
+      value.push(format(startOfCycle, "yyyy-MM-dd"));
+
+      for (let i = 1; i < cycle.periodLength; i++) {
         startOfCycle.setDate(startOfCycle.getDate() + 1);
         value.push(format(startOfCycle, "yyyy-MM-dd"));
       }
@@ -99,10 +102,42 @@ const EditModal = (props: PropsEditModal) => {
             <IonButton
               color="basic"
               onClick={() => {
-                // datetimeRef.current?.confirm().catch((err) => console.error(err));
-                // props.setIsOpen(false);
-                // console.log("Save editing");
-                console.log("var = ", periodDays);
+                if (periodDays.length > 0) {
+                  const millisecondsInDay = 24 * 60 * 60 * 1000;
+                  periodDays.sort();
+                  const newCycles: Cycle[] = [
+                    {
+                      cycleLength: 28,
+                      periodLength: 1,
+                      startDate: periodDays[0],
+                    },
+                  ];
+                  for (let i = 1; i < periodDays.length; i++) {
+                    const date = new Date(periodDays[i]);
+                    const prevDate = new Date(periodDays[i - 1]);
+                    const diffInDays = Math.abs(
+                      (date.getTime() - prevDate.getTime()) / millisecondsInDay,
+                    );
+
+                    if (diffInDays < 2) {
+                      newCycles[0].periodLength++;
+                    } else {
+                      newCycles[0].cycleLength =
+                        diffInDays + newCycles[0].periodLength - 1;
+                      newCycles.unshift({
+                        cycleLength: 0,
+                        periodLength: 1,
+                        startDate: periodDays[i],
+                      });
+                    }
+                  }
+                  updateCycles(newCycles);
+                }
+                datetimeRef.current
+                  ?.confirm()
+                  .catch((err) => console.error(err));
+                props.setIsOpen(false);
+                console.log("Save editing");
               }}
             >
               {t("save")}
