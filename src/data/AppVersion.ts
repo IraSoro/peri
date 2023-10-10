b5859fddc394267af216674d9d0cb41db8175767
Irina Sorokina <irasoro402@yandex.ru>
2023-10-11 01:55:10 +0400

    Updated version

--- src/data/AppVersion.ts
+++ src/data/AppVersion.ts
@@ -1,6 +1,6 @@
 import { isPlatform } from "@ionic/core";
 
-export const appVersion = "v2.3.1";
+export const appVersion = "v2.3.2";
 
 export interface GithubReleaseAsset {
   content_type: string;
