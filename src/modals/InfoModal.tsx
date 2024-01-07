a2eba9f2fc37751837ee00f5303a1482c660592d
Irina Sorokina <irasoro402@yandex.ru>
2024-01-08 00:27:26 +0400

    Added basic `Swiper` in the `Info Modal`

--- src/modals/InfoModal.tsx
+++ src/modals/InfoModal.tsx
@@ -2,6 +2,13 @@ import { useContext } from "react";
 import { IonContent, IonModal, IonButton, IonCol } from "@ionic/react";
 import { useTranslation } from "react-i18next";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Scrollbar, A11y } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+
 import { CyclesContext, ThemeContext } from "../state/Context";
 import {
   getAverageLengthOfCycle,
@@ -34,116 +41,127 @@ const InfoModal = (props: PropsInfoModal) => {
       backdropDismiss={false}
       isOpen={props.isOpen}
     >
-      <IonContent
-        className="ion-padding"
-        color={`transparent-${theme}`}
-      >
-        <p
-          style={{
-            fontWeight: "bold",
-            fontSize: "25px",
-            color: `var(--ion-color-dark-${theme})`,
-            marginBottom: "24px",
-          }}
+      <IonContent color={`transparent-${theme}`}>
+        <Swiper
+          modules={[Navigation, Scrollbar, A11y]}
+          navigation={true} // NOTE: for tests
+          scrollbar={true}
         >
-          {`${t("Days", {
-            postProcess: "interval",
-            count: 1, // NOTE: to indicate which day is in the account, you need to write the day as if in the singular
-          })} `}
-          {cycles.length === 1 ? (
-            currentDay
-          ) : (
-            <>
-              {currentDay}/{lengthOfCycle}
-            </>
-          )}
-        </p>
-        <ul>
-          <li
-            style={{
-              fontSize: "16px",
-              color: `var(--ion-color-text-${theme})`,
-              marginBottom: "20px",
-            }}
-          >
-            <span
+          <SwiperSlide>
+            <p
               style={{
-                color: `var(--ion-color-less-dark-${theme})`,
                 fontWeight: "bold",
+                fontSize: "25px",
+                color: `var(--ion-color-dark-${theme})`,
+                marginBottom: "24px",
               }}
             >
-              {phase.title}
-            </span>
-            <span> {t("is current phase of cycle")}</span>
-          </li>
-          <li
-            style={{
-              fontSize: "16px",
-              color: `var(--ion-color-text-${theme})`,
-              marginBottom: "20px",
-            }}
-          >
-            <span>{t("Ovulation")}</span>
-            <span
-              style={{
-                color: `var(--ion-color-less-dark-${theme})`,
-                fontWeight: "bold",
-              }}
-            >
-              {` ${ovulationStatus}`}
-            </span>
-          </li>
-          <li
-            style={{
-              fontSize: "16px",
-              color: `var(--ion-color-text-${theme})`,
-              marginBottom: "20px",
-            }}
-          >
-            <span
-              style={{
-                color: `var(--ion-color-less-dark-${theme})`,
-                fontWeight: "bold",
-              }}
-            >
-              {pregnancyChance}
-            </span>
-            <span> {t("chance of getting pregnant")}</span>
-          </li>
-        </ul>
-        <p
-          style={{
-            fontWeight: "bold",
-            fontSize: "25px",
-            color: `var(--ion-color-dark-${theme})`,
-            marginBottom: "24px",
-          }}
-        >
-          {t("Frequent symptoms")}
-        </p>
-        <ul>
-          {phase.symptoms.map((item, idx) => (
-            <li
-              style={{
-                fontSize: "16px",
-                color: `var(--ion-color-text-${theme})`,
-                marginBottom: "20px",
-              }}
-              key={idx}
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
-        <IonCol>
-          <IonButton
-            className="main"
-            color={`dark-${theme}`}
-            onClick={() => props.setIsOpen(false)}
-          >
-            OK
-          </IonButton>
-        </IonCol>
+              {`${t("Days", {
+                postProcess: "interval",
+                count: 1, // NOTE: to indicate which day is in the account, you need to write the day as if in the singular
+              })} `}
+              {cycles.length === 1 ? (
+                currentDay
+              ) : (
+                <>
+                  {currentDay}/{lengthOfCycle}
+                </>
+              )}
+            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ul>
+              <li
+                style={{
+                  fontSize: "16px",
+                  color: `var(--ion-color-text-${theme})`,
+                  marginBottom: "20px",
+                }}
+              >
+                <span
+                  style={{
+                    color: `var(--ion-color-less-dark-${theme})`,
+                    fontWeight: "bold",
+                  }}
+                >
+                  {phase.title}
+                </span>
+                <span> {t("is current phase of cycle")}</span>
+              </li>
+              <li
+                style={{
+                  fontSize: "16px",
+                  color: `var(--ion-color-text-${theme})`,
+                  marginBottom: "20px",
+                }}
+              >
+                <span>{t("Ovulation")}</span>
+                <span
+                  style={{
+                    color: `var(--ion-color-less-dark-${theme})`,
+                    fontWeight: "bold",
+                  }}
+                >
+                  {` ${ovulationStatus}`}
+                </span>
+              </li>
+              <li
+                style={{
+                  fontSize: "16px",
+                  color: `var(--ion-color-text-${theme})`,
+                  marginBottom: "20px",
+                }}
+              >
+                <span
+                  style={{
+                    color: `var(--ion-color-less-dark-${theme})`,
+                    fontWeight: "bold",
+                  }}
+                >
+                  {pregnancyChance}
+                </span>
+                <span> {t("chance of getting pregnant")}</span>
+              </li>
+            </ul>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div>
+              <p
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "25px",
+                  color: `var(--ion-color-dark-${theme})`,
+                  marginBottom: "24px",
+                }}
+              >
+                {t("Frequent symptoms")}
+              </p>
+              <ul>
+                {phase.symptoms.map((item, idx) => (
+                  <li
+                    style={{
+                      fontSize: "16px",
+                      color: `var(--ion-color-text-${theme})`,
+                      marginBottom: "20px",
+                    }}
+                    key={idx}
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
+              <IonCol>
+                <IonButton
+                  className="main"
+                  color={`dark-${theme}`}
+                  onClick={() => props.setIsOpen(false)}
+                >
+                  OK
+                </IonButton>
+              </IonCol>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </IonContent>
     </IonModal>
   );
