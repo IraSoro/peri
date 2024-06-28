e111d8ee139c2a664bb25bb57f071eb93d2f7d56
Irina Sorokina <irasoro402@yandex.ru>
2024-06-28 19:16:12 +0400

    Updates

--- static/js/5892.6f00b1d3.chunk.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 5892.6f00b1d3.chunk.js.LICENSE.txt */
-"use strict";(self.webpackChunkperi=self.webpackChunkperi||[]).push([[5892],{827:function(n,r,e){e.r(r),e.d(r,{g:function(){return u}});var i=e(3023),u=function(){if(void 0!==i.w)return i.w.Capacitor}}}]);
-//# sourceMappingURL=5892.6f00b1d3.chunk.js.map
