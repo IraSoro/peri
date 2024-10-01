9127222cc9359d1d0f5bb9be7e10429092b9f894
Whirl <ninja21official@gmail.com>
2024-10-01 10:20:39 +0000

    add de transalations

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -5,6 +5,8 @@ import intervalPlural from "i18next-intervalplural-postprocessor";
 import en from "./translations/en";
 import ru from "./translations/ru";
 import es from "./translations/es";
+import de from "./translations/de";
+
 
 import { storage } from "../data/Storage";
 
@@ -12,6 +14,8 @@ export const supportedLanguages = new Map([
   ["en", "english"],
   ["ru", "русский"],
   ["es", "español"],
+  ["de", "deutsch"],
+  
 ]);
 
 const defaultLanguageCode = "en";
@@ -32,11 +36,15 @@ export async function init() {
         es: {
           translation: es,
         },
+        de: {
+          translation: de,
+        },
       },
       lng: (await storage.getUnsafe.language()) || navigator.language,
       fallbackLng: {
         ru: ["ru"],
         es: ["es"],
+        de: ["de"],
         default: [defaultLanguageCode],
       },
     } satisfies InitOptions);
