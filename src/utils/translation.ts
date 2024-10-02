d9826ea3545e1768aa34a989e9fd4ba638e248cc
Irina Sorokina <irasoro402@yandex.ru>
2024-10-03 00:32:41 +0400

    Added language to `fallbackLng`

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -42,6 +42,7 @@ export async function init() {
       fallbackLng: {
         ru: ["ru"],
         es: ["es"],
+        hg: ["hg"],
         default: [defaultLanguageCode],
       },
     } satisfies InitOptions);
