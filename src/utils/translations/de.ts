2a58332a69e65e4ed05835af32463525dfc04355
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2024-10-22 14:29:27 +0530

    Created de.ts
    Added German translation

---
+++ src/utils/translations/de.ts
@@ -0,0 +1,95 @@
+const de = {
+  // Home Tab
+  Home: "Startseite",
+  "Period in": "Monatsblutung in",
+  "no info": "unbekannt",
+  Period: "Monatsblutung",
+  today: "heute",
+  Delay: "Verspätung",
+  Days: "(0)[Tage];(1)[Tag];(2-4)[Tage];(5-20)[Tagen];(21)[Tag];(22-24)[Tage];(25-30)[Tagen];(31)[Tag];(32-34)[Tage];(35-40)[Tagen];",
+  "Current cycle day": "Aktueller Zyklustag",
+  day: "{{count}}-ter Tag",
+  Ovulation: "Ovulation",
+  possible: "kann heute sein",
+  finished: "abgeschlossen",
+  tomorrow: "morgen",
+  in: "in",
+  "chance of getting pregnant": "Schwangerschaftswahrscheinlichkeit",
+  High: "Hoch",
+  Low: "Niedrig",
+  "Period today": "Monatsblutung heute",
+  edit: "bearbeiten",
+  save: "speichern",
+  "Period is": "Monatsblutung ist",
+  "possible today": "möglich heute",
+  // Details Tab
+  Details: "Details",
+  "Period length": "Dauer der Monatsblutung",
+  "Cycle length": "Zykluslänge",
+  "You haven't marked any periods yet": "Sie haben noch keine Tage der Monatsblutung markiert",
+  // Mark Modal
+  mark: "markieren",
+  cancel: "Abbrechen",
+  // Welcome Modal
+  "Welcome to Peri": "Willkommen bei Peri",
+  "Mark the days of your": "Markieren Sie die Tage Ihrer",
+  "last period": "letzten Monatsblutung",
+  Continue: "Fortsetzen",
+  "Forecast will not be generated.": "Vorhersage wird nicht generiert.",
+  or: "oder",
+  // Info Modal
+  "Frequent symptoms": "Häufige Symptome",
+  "is current phase of cycle": "- aktuelle Phase des Zyklus",
+  // Phases info
+  "Menstrual phase": "Menstruationsphase",
+  "This cycle is accompanied by low hormone levels.":
+    "Dieser Zyklus wird von niedrigen Hormonspiegeln begleitet.",
+  "lack of energy and strength": "Mangel an Energie und Kraft",
+  pain: "Schmerz",
+  "weakness and irritability": "Schwäche und Reizbarkeit",
+  "increased appetite": "erhöhter Appetit",
+
+  "Follicular phase": "Follikuläre Phase",
+  "The level of estrogen in this phase rises and reaches a maximum level.":
+    "Der Östrogenspiegel steigt in dieser Phase und erreicht einen Höchstwert.",
+  "strength and vigor appear": "Kraft und Vitalität erscheinen",
+  "endurance increases": "Ausdauer nimmt zu",
+  "new ideas and plans appear": "neue Ideen und Pläne erscheinen",
+  "libido increases": "Libido steigt",
+
+  "Ovulation phase": "Ovulationsphase",
+  "Once estrogen levels peak, they trigger the release of two important ovulation hormones, follicle-stimulating hormone and luteinizing hormone.":
+    "Sobald die Östrogenspiegel ihren Höhepunkt erreichen, lösen sie die Freisetzung von zwei wichtigen Ovulationshormonen aus, dem follikelstimulierenden Hormon und dem luteinisierenden Hormon.",
+  "increased sexual desire": "erhöhtes sexuelles Verlangen",
+  "optimistic mood": "optimistische Stimmung",
+  "mild fever": "leichte Fieber",
+  "lower abdominal pain": "Schmerzen im Unterbauch",
+  "chest discomfort and bloating": "Brustbeschwerden und Blähungen",
+  "characteristic secretions": "charakteristische Absonderungen",
+
+  "Luteal phase": "Luteale Phase",
+  "Levels of the hormones estrogen and progesterone first rise and then drop sharply just before a period. Progesterone reaches its peak in the luteal phase.":
+    "Die Werte der Hormone Östrogen und Progesteron steigen zunächst an und fallen dann kurz vor der Monatsblutung stark ab. Progesteron erreicht in der lutealen Phase seinen Höhepunkt.",
+  "breast tenderness": "Brustempfindlichkeit",
+  puffiness: "Schwellung",
+  "acne and skin rashes": "Akne und Hautausschläge",
+  "diarrhea or constipation": "Durchfall oder Verstopfung",
+  "irritability and depressed mood":
+    "Reizbarkeit und gedrückte Stimmung",
+  // Menu
+  Preferences: "Einstellungen",
+  Edit: "Bearbeiten",
+  Language: "Sprache",
+  Theme: "Thema",
+  "Import config": "Daten importieren",
+  "Export config": "Daten exportieren",
+  "Configuration has been imported": "Daten wurden erfolgreich importiert",
+  "Download latest version": "Neueste Version herunterladen",
+  // Alert Demo
+  "This is just a demo": "Das ist nur eine Demo",
+  "You can download the application ": "Die Anwendung kann heruntergeladen werden ",
+  here: "hier",
+  "We are on GitHub": "Wir sind auf GitHub"
+};
+
+export default de;
