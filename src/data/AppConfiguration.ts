57118d45081f72bab86bab40dbe6559250bab292
Irina Sorokina <--help>
2024-11-14 16:37:12 +0400

    Renamed features flag

--- src/data/AppConfiguration.ts
+++ src/data/AppConfiguration.ts
@@ -6,6 +6,6 @@ export const configuration = {
     useCustomVersionUpdate:
       process.env.REACT_APP_FEATURE_USE_CUSTOM_VERSION_UPDATE === "true",
     demoMode: process.env.REACT_APP_FEATURE_DEMO_MODE === "true",
-    moreLanguages: process.env.REACT_APP_FEATURE_MORE_LANGUAGES === "true",
+    betaLanguages: process.env.REACT_APP_FEATURE_BETA_LANGUAGES === "true",
   },
 };
