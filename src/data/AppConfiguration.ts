78c69e5bd91ad5e6ca5f9917cdc99fd6be664e95
Irina Sorokina <--help>
2024-12-05 15:17:33 +0400

    Moved `.env`

--- src/data/AppConfiguration.ts
+++ src/data/AppConfiguration.ts
@@ -1,11 +1,11 @@
 export const configuration = {
   app: {
-    version: process.env.REACT_APP_APP_VERSION || "",
+    version: import.meta.env.VITE_APP_VERSION || "",
   },
   features: {
     useCustomVersionUpdate:
-      process.env.REACT_APP_FEATURE_USE_CUSTOM_VERSION_UPDATE === "true",
-    demoMode: process.env.REACT_APP_FEATURE_DEMO_MODE === "true",
-    betaLanguages: process.env.REACT_APP_FEATURE_BETA_LANGUAGES === "true",
+    import.meta.env.VITE_FEATURE_USE_CUSTOM_VERSION_UPDATE === "true",
+    demoMode: import.meta.env.VITE_FEATURE_DEMO_MODE === "true",
+    betaLanguages: import.meta.env.VITE_FEATURE_BETA_LANGUAGES === "true",
   },
 };
