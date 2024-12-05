95d4c27dba944b66fb426781b024e553d99593fb
Irina Sorokina <--help>
2024-12-05 23:08:25 +0400

    Deleted comments

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), legacy()],
-  // build: {
-  //   minify: "terser",
-  // },
   server: {
     open: true,
     port: 3000,
