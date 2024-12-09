bf04bc2b05e23b15d5f60de9a768e5b27a81197f
Irina Sorokina <--help>
2024-12-09 23:09:09 +0400

    Changed some files for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
+/// <reference types="vitest" />
+
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react(), legacy()],
-  server: {
-    open: true,
-    port: 3000,
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   },
-});
+}));
