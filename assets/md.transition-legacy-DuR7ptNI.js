2439dd3ec8469a034029320ddf2fbdc57c0b84f4
Irina Sorokina <irasoro402@yandex.ru>
2025-01-18 01:26:13 +0400

    Updates

--- assets/md.transition-legacy-DuR7ptNI.js
+++ assets/md.transition-legacy-DuR7ptNI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DNp3xruc.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
+System.register(["./index-legacy-DAsyWGtX.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
