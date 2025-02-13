df842f140a9f5e02ad0d2a2b5e7094d0e54ea861
Yury Getman <imblowfish@duck.com>
2025-02-14 00:28:01 +0400

    Replaced `@ionic/storage` with `@capacitor/preferences` in the storage

--- src/tests/Storage.test.ts
+++ src/tests/Storage.test.ts
@@ -1,13 +1,21 @@
-// import * as mockedIonicStorage from "@ionic/storage";
 import { it, expect, vi, describe } from "vitest";
-import { Storage } from "@ionic/storage";
+import { Preferences } from "@capacitor/preferences";
+import { storage, StorageKey } from "../data/Storage";
 
-import { storage } from "../data/Storage";
+vi.mock("@capacitor/preferences", () => {
+  return {
+    Preferences: {
+      set: vi.fn(),
+      get: vi.fn(),
+    },
+  };
+});
 
 it("setCycles", async () => {
-  const storageSetSpy = vi
-    .spyOn(Storage.prototype, "set")
-    .mockResolvedValueOnce({});
+  console.log(Preferences);
+  const preferencesSetSpy = vi
+    .spyOn(Preferences, "set")
+    .mockResolvedValueOnce();
 
   const cycles = [
     {
@@ -18,20 +26,22 @@ it("setCycles", async () => {
   ];
 
   await storage.set.cycles(cycles);
-
-  expect(storageSetSpy).toHaveBeenCalledWith("cycles", cycles);
+  expect(preferencesSetSpy).toHaveBeenCalledWith({
+    key: StorageKey.Cycles,
+    value: JSON.stringify(cycles),
+  });
 });
 
 describe("getCycles", () => {
   it("There are no cycles in storage", async () => {
-    const storageGetSpy = vi
-      .spyOn(Storage.prototype, "get")
-      .mockResolvedValueOnce(undefined);
+    const preferencesGetSpy = vi
+      .spyOn(Preferences, "get")
+      .mockResolvedValueOnce({ value: null });
 
     await expect(storage.get.cycles()).rejects.toThrow(
-      "Can't find `cycles` in storage",
+      "Can't find 'cycles' in storage",
     );
-    expect(storageGetSpy).toHaveBeenCalledWith("cycles");
+    expect(preferencesGetSpy).toHaveBeenCalledWith({ key: StorageKey.Cycles });
   });
 
   it("Storage has cycles", async () => {
@@ -43,65 +53,72 @@ describe("getCycles", () => {
       },
     ];
 
-    const storageGetSpy = vi
-      .spyOn(Storage.prototype, "get")
-      .mockResolvedValueOnce(cycles);
+    const preferencesGetSpy = vi
+      .spyOn(Preferences, "get")
+      .mockResolvedValueOnce({ value: JSON.stringify(cycles) });
 
-    await expect(storage.get.cycles()).resolves.toBe(cycles);
-    expect(storageGetSpy).toHaveBeenCalledWith("cycles");
+    await expect(storage.get.cycles()).resolves.toStrictEqual(cycles);
+    expect(preferencesGetSpy).toHaveBeenCalledWith({ key: StorageKey.Cycles });
   });
 });
 
 it("getCyclesUnsafe", async () => {
-  const storageGetSpy = vi
-    .spyOn(Storage.prototype, "get")
-    .mockResolvedValueOnce(undefined);
+  const preferencesGetSpy = vi
+    .spyOn(Preferences, "get")
+    .mockResolvedValueOnce({ value: null });
 
-  await expect(storage.getUnsafe.cycles()).resolves.toBe(undefined);
-  expect(storageGetSpy).toHaveBeenCalledWith("cycles");
+  await expect(storage.getUnsafe.cycles()).resolves.toBe(null);
+  expect(preferencesGetSpy).toHaveBeenCalledWith({ key: StorageKey.Cycles });
 });
 
 it("setLanguage", async () => {
-  const storageSetSpy = vi
-    .spyOn(Storage.prototype, "set")
-    .mockResolvedValueOnce({});
+  const preferencesSetSpy = vi
+    .spyOn(Preferences, "set")
+    .mockResolvedValueOnce();
 
   const language = "en";
 
   await storage.set.language(language);
 
-  expect(storageSetSpy).toHaveBeenCalledWith("language", language);
+  expect(preferencesSetSpy).toHaveBeenCalledWith({
+    key: StorageKey.Language,
+    value: language,
+  });
 });
 
 describe("getLanguage", () => {
   it("There are no language in storage", async () => {
-    const storageGetSpy = vi
-      .spyOn(Storage.prototype, "get")
-      .mockResolvedValueOnce(undefined);
+    const preferencesGetSpy = vi
+      .spyOn(Preferences, "get")
+      .mockResolvedValueOnce({ value: null });
 
     await expect(storage.get.language()).rejects.toThrow(
-      "Can't find `language` in storage",
+      "Can't find 'language' in storage",
     );
-    expect(storageGetSpy).toHaveBeenCalledWith("language");
+    expect(preferencesGetSpy).toHaveBeenCalledWith({
+      key: StorageKey.Language,
+    });
   });
 
   it("Storage has language", async () => {
     const language = "en";
 
-    const storageGetSpy = vi
-      .spyOn(Storage.prototype, "get")
-      .mockResolvedValueOnce(language);
+    const preferencesGetSpy = vi
+      .spyOn(Preferences, "get")
+      .mockResolvedValueOnce({ value: language });
 
     await expect(storage.get.language()).resolves.toBe(language);
-    expect(storageGetSpy).toHaveBeenCalledWith("language");
+    expect(preferencesGetSpy).toHaveBeenCalledWith({
+      key: StorageKey.Language,
+    });
   });
 });
 
 it("getLanguageUnsafe", async () => {
-  const storageGetSpy = vi
-    .spyOn(Storage.prototype, "get")
-    .mockResolvedValueOnce(undefined);
+  const preferencesGetSpy = vi
+    .spyOn(Preferences, "get")
+    .mockResolvedValueOnce({ value: null });
 
-  await expect(storage.getUnsafe.language()).resolves.toBe(undefined);
-  expect(storageGetSpy).toHaveBeenCalledWith("language");
+  await expect(storage.getUnsafe.language()).resolves.toBe(null);
+  expect(preferencesGetSpy).toHaveBeenCalledWith({ key: StorageKey.Language });
 });
