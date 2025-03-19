65cd0636be294e6f08c786e42944185bd32df566
Irina Sorokina <irasoro402@yandex.ru>
2025-03-19 22:54:32 +0400

    Updates

--- assets/md.transition-legacy-CleG8xZz.js
+++ assets/md.transition-legacy-CleG8xZz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-7C2XgYhG.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
+System.register(["./index-legacy-D7bov30r.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
