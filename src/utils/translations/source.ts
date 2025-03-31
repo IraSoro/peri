3d71150c24eafbd1f95f87cb11abb132e76c93c8
Irina Sorokina <irasoro402@yandex.ru>
2025-03-31 17:54:44 +0400

    Added explanation to translation

---
+++ src/utils/translations/source.ts
@@ -0,0 +1,115 @@
+const example = {
+  // Home Tab
+  Home: "",
+  "Period in": "", // eg. Period in 5 Days
+  "no info": "",
+  Period: "", // eg. Period today
+  today: "",
+  Delay: "", // mean Period Delay
+  Days: "", // eg. "(0)[Days];(1)[Day];(2-inf)[Days];"
+  "Current cycle day": "",
+
+  // You can write like this (general way):
+  // day: "{{count}}. päivä" (päivä is day)
+  // or
+  // day: "{{count}}-й день" (день is day)
+  // If language has specific rules then it's like this (see en.ts):
+  // day_ordinal_one: "{{count}}st day",
+  // day_ordinal_two: "{{count}}nd day",
+  // day_ordinal_few: "{{count}}rd day",
+  // day_ordinal_other: "{{count}}th day",
+  day: "", // eg. Period - 1st day
+  Ovulation: "",
+  possible: "", // eg. Ovulation - possible
+  finished: "", // eg. Ovulation - finished
+  tomorrow: "", // eg. Ovulation - tomorrow
+  in: "", // eg. Period in 5 Days
+  "chance of getting pregnant": "", // eg. High/Low chance of getting pregnant
+  High: "",
+  Low: "",
+  "Period today": "",
+  edit: "",
+  save: "",
+  "Period is": "", // eg. Period is possible today
+  "possible today": "",
+
+  // Details Tab
+  Details: "",
+  "Period length": "", // eg. Period length - 5 Days
+  "Cycle length": "", // eg. Cycle length - 25 Days
+  "You haven't marked any periods yet": "",
+
+  // Mark Modal
+  mark: "", // eg. mark your period
+  cancel: "",
+
+  // Welcome Modal
+  "Welcome to Peri": "",
+  "Mark the days of your": "", // eg. Mark the days of your last period
+  "last period": "", // eg. Mark the days of your last period
+  Continue: "",
+  "Forecast will not be generated.": "",
+  or: "",
+
+  // Info Modal
+  "Frequent symptoms": "",
+  "is current phase of cycle": "", // eg. Menstrual phase is current phase of cycle"
+
+  // Phases info
+  "Menstrual phase": "",
+  "This cycle is accompanied by low hormone levels.": "",
+  "lack of energy and strength": "",
+  pain: "",
+  "weakness and irritability": "",
+  "increased appetite": "",
+
+  "Follicular phase": "",
+  "The level of estrogen in this phase rises and reaches a maximum level.": "",
+  "strength and vigor appear": "",
+  "endurance increases": "",
+  "new ideas and plans appear": "",
+  "libido increases": "",
+
+  "Ovulation phase": "",
+  "Once estrogen levels peak, they trigger the release of two important ovulation hormones, follicle-stimulating hormone and luteinizing hormone.":
+    "",
+  "increased sexual desire": "",
+  "optimistic mood": "",
+  "mild fever": "",
+  "lower abdominal pain": "",
+  "chest discomfort and bloating": "",
+  "characteristic secretions ": "",
+
+  "Luteal phase": "",
+  "Levels of the hormones estrogen and progesterone first rise and then drop sharply just before a period. Progesterone reaches its peak in the luteal phase.":
+    "",
+  "breast tenderness": "",
+  puffiness: "",
+  "acne and skin rashes": "",
+  "diarrhea or constipation": "",
+  "irritability and depressed mood": "",
+
+  // Menu
+  Preferences: "",
+  Edit: "",
+  Language: "",
+  Theme: "",
+  "Import config": "",
+  "Export config": "",
+  "Configuration has been imported": "",
+  "Download latest version": "",
+  "We are on GitHub": "",
+
+  // Alert Demo
+  "This is just a demo": "",
+  "You can download the application ": "", // eg. You can download the application here
+  here: "", // eg. You can download the application here
+
+  // Notifications
+  Notifications: "",
+  "Period is coming soon": "",
+  "Your period may start tomorrow": "",
+  "Your period may start today": "",
+};
+
+export default example;
