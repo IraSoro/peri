6b759a2c38975d3705f08d00b897ee27a21f7872
Irina Sorokina <irasoro402@yandex.ru>
2025-04-02 00:08:41 +0400

    Fixed other bugs

--- src/App.tsx
+++ src/App.tsx
@@ -108,7 +108,7 @@ const App = (props: AppProps) => {
       });
       removePendingNotifications()
         .then(() => {
-          createNotifications(cycles).catch((err) => {
+          createNotifications(cycles, maxDisplayedCycles).catch((err) => {
             console.error("Error creating notifications", err);
           });
         })
@@ -135,7 +135,7 @@ const App = (props: AppProps) => {
           `Notification has been switched to ${newStatus ? "on" : "off"}`,
         );
         if (newStatus) {
-          createNotifications(cycles).catch((err) => {
+          createNotifications(cycles, maxDisplayedCycles).catch((err) => {
             console.error("Error creating notifications", err);
           });
           return;
