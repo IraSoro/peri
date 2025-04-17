6516fd3d8dd036bf79d42503efc6db30f99e7dbd
Irina Sorokina <irasoro402@yandex.ru>
2025-04-17 23:46:48 +0400

    Updates

--- assets/md.transition-legacy-BT-GpFzD.js
+++ assets/md.transition-legacy-BT-GpFzD.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-HtVVaQAB.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
+System.register(["./index-legacy-B2sU5RGK.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
