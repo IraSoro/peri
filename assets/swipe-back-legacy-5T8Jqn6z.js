67db9ad195bb6722bd096c2b3c5deab752b228ee
Irina Sorokina <irasoro402@yandex.ru>
2025-04-17 23:23:03 +0400

    Updates

--- assets/swipe-back-legacy-5T8Jqn6z.js
+++ assets/swipe-back-legacy-5T8Jqn6z.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D7bov30r.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-rYSboP_e.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
