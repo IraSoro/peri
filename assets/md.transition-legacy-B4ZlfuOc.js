3228680d3eab952b95cc28f92b8516b873367f32
Irina Sorokina <irasoro402@yandex.ru>
2025-07-27 23:17:20 +0400

    Updates

--- assets/md.transition-legacy-B4ZlfuOc.js
+++ assets/md.transition-legacy-B4ZlfuOc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B2sU5RGK.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
+System.register(["./index-legacy-zaPSdvD0.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.o,i=n.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Cj-tG8nI.js
+++ assets/swipe-back-legacy-Cj-tG8nI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B2sU5RGK.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-zaPSdvD0.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
