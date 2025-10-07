35e906da09cb78d0bac6d7bf5952512224900af4
Bhuwan <133322497+BhuwanSKumar@users.noreply.github.com>
2025-10-07 13:01:00 +0530

    Fix French translation errors

--- src/utils/translations/fr.ts
+++ src/utils/translations/fr.ts
@@ -2,14 +2,14 @@ const fr = {
   // Home Tab
   Home: "Accueil",
   "Period in": "Règles dans",
-  "no info": "inconnu",
+  "no info": "Aucune info",
   Period: "Règles",
   today: "aujourd'hui",
   Delay: "Retard",
   Days: "(0)[Jour];(1)[Jour];(2-inf)[Jours];",
   "Current cycle day": "Jour actuel du cycle",
-  day_ordinal_one: "{{count}} r jour",
-  day_ordinal_other: "{{count}} me jour",
+  day_ordinal_one: "{{count}}er jour",
+  day_ordinal_other: "{{count}}e jour",
   Ovulation: "Ovulation",
   possible: "peut-être aujourd'hui",
   finished: "terminée",
@@ -23,15 +23,17 @@ const fr = {
   save: "enregistrer",
   "Period is": "Les règles sont",
   "possible today": "possibles aujourd'hui",
+
   // Details Tab
   Details: "Détails",
   "Period length": "Durée des règles",
   "Cycle length": "Durée du cycle",
-  "You haven't marked any periods yet":
-    "Vous n'avez pas encore marqué de jours de règles",
+  "You haven't marked any periods yet": "Vous n'avez pas encore marqué de jours de règles",
+
   // Mark Modal
   mark: "marquer",
   cancel: "annuler",
+
   // Welcome Modal
   "Welcome to Peri": "Bienvenue sur Peri",
   "Mark the days of your": "Marquez les jours de vos",
@@ -39,29 +41,28 @@ const fr = {
   Continue: "Continuer",
   "Forecast will not be generated.": "La prévision ne sera pas générée.",
   or: "ou",
+
   // Info Modal
   "Frequent symptoms": "Symptômes fréquents",
   "is current phase of cycle": "- phase actuelle du cycle",
+
   // Phases info
   "Menstrual phase": "Phase menstruelle",
-  "This cycle is accompanied by low hormone levels.":
-    "Ce cycle est accompagné de faibles niveaux d'hormones.",
+  "This cycle is accompanied by low hormone levels.": "Ce cycle est accompagné de faibles niveaux d'hormones.",
   "lack of energy and strength": "manque d'énergie et de force",
   pain: "douleur",
   "weakness and irritability": "faiblesse et irritabilité",
   "increased appetite": "appétit accru",
 
   "Follicular phase": "Phase folliculaire",
-  "The level of estrogen in this phase rises and reaches a maximum level.":
-    "Le niveau d'œstrogène dans cette phase augmente et atteint un niveau maximal.",
+  "The level of estrogen in this phase rises and reaches a maximum level.": "Le niveau d'œstrogène augmente dans cette phase et atteint son maximum.",
   "strength and vigor appear": "force et vigueur apparaissent",
   "endurance increases": "l'endurance augmente",
-  "new ideas and plans appear": "de nouvelles idées et plans apparaissent",
+  "new ideas and plans appear": "de nouvelles idées et projets apparaissent",
   "libido increases": "la libido augmente",
 
   "Ovulation phase": "Phase d'ovulation",
-  "Once estrogen levels peak, they trigger the release of two important ovulation hormones, follicle-stimulating hormone and luteinizing hormone.":
-    "Une fois que les niveaux d'œstrogène atteignent leur maximum, ils déclenchent la libération de deux hormones importantes de l'ovulation, l'hormone folliculo-stimulante et l'hormone lutéinisante.",
+  "Once estrogen levels peak, they trigger the release of two important ovulation hormones, follicle-stimulating hormone and luteinizing hormone.": "Lorsque les niveaux d'œstrogène atteignent leur maximum, ils déclenchent la libération de deux hormones importantes pour l'ovulation : l'hormone folliculo-stimulante et l'hormone lutéinisante.",
   "increased sexual desire": "désir sexuel accru",
   "optimistic mood": "humeur optimiste",
   "mild fever": "fièvre légère",
@@ -70,13 +71,13 @@ const fr = {
   "characteristic secretions": "sécrétions caractéristiques",
 
   "Luteal phase": "Phase lutéale",
-  "Levels of the hormones estrogen and progesterone first rise and then drop sharply just before a period. Progesterone reaches its peak in the luteal phase.":
-    "Les niveaux des hormones œstrogène et progestérone augmentent d'abord puis chutent brusquement juste avant les règles. La progestérone atteint son pic dans la phase lutéale.",
+  "Levels of the hormones estrogen and progesterone first rise and then drop sharply just before a period. Progesterone reaches its peak in the luteal phase.": "Les niveaux d'œstrogène et de progestérone augmentent d'abord, puis chutent brusquement juste avant les règles. La progestérone atteint son pic pendant la phase lutéale.",
   "breast tenderness": "sensibilité des seins",
   puffiness: "gonflement",
   "acne and skin rashes": "acné et éruptions cutanées",
   "diarrhea or constipation": "diarrhée ou constipation",
   "irritability and depressed mood": "irritabilité et humeur dépressive",
+
   // Menu
   Preferences: "Préférences",
   Edit: "Modifier",
@@ -84,9 +85,9 @@ const fr = {
   Theme: "Thème",
   "Import config": "Importer les données",
   "Export config": "Exporter les données",
-  "Configuration has been imported":
-    "Les données ont été importées avec succès",
+  "Configuration has been imported": "Les données ont été importées avec succès",
   "Download latest version": "Télécharger la dernière version",
+
   // Alert Demo
   "This is just a demo": "Ceci est juste une démo",
   "You can download the application ": "Vous pouvez télécharger l'application ",
