7bc69c8c88e826c801e22bf866c6f5a47486f732
Ewelina Bierć <erbierc@gmail.com>
2025-10-11 19:13:26 +0200

    feat(i18n): Add polish translation file

---
+++ src/utils/translations/pl.ts
@@ -0,0 +1,126 @@
+const pl = {
+  // Home Tab
+  Home: "Strona główna",
+  "Period in": "Miesiączka za", // eg. Period in 5 Days
+  "no info": "brak danych",
+  Period: "Miesiączka", // eg. Period today
+  today: "dzisiaj",
+  Delay: "Opóźnienie", // mean Period Delay
+  Days: "Dni", // eg. "(0)[Days];(1)[Day];(2-inf)[Days];"
+  "Current cycle day": "Obecny dzień cyklu",
+
+  // You can write like this (general way):
+  // day: "{{count}}. päivä" (päivä is day)
+  // or
+  // day: "{{count}}-й день" (день is day)
+  // If language has specific rules then it's like this (see en.ts):
+  // day_ordinal_one: "{{count}}st day",
+  // day_ordinal_two: "{{count}}nd day",
+  // day_ordinal_few: "{{count}}rd day",
+  // day_ordinal_other: "{{count}}th day",
+  day: "{{count}}. dzień", // eg. Period - 1st day
+  Ovulation: "Owulacja",
+  possible: "możliwa", // eg. Ovulation - possible
+  finished: "zakończona", // eg. Ovulation - finished
+  tomorrow: "jutro", // eg. Ovulation - tomorrow
+  in: "za", // eg. Period in 5 Days
+  "chance of getting pregnant": "szansa na zajście w ciążę", // eg. High/Low chance of getting pregnant
+  High: "Wysoka",
+  Low: "Niska",
+  "Period today": "Dzisiaj miesiączka",
+  edit: "edytuj",
+  save: "zapisz",
+  "Period is": "Miesiączka jest", // eg. Period is possible today
+  "possible today": "możliwa dzisiaj",
+
+  // Details Tab
+  Details: "Szczegóły",
+  "Period length": "Długość miesiączki", // eg. Period length - 5 Days
+  "Cycle length": "Długość cyklu", // eg. Cycle length - 25 Days
+  "You haven't marked any periods yet":
+    "Nie oznaczono jeszcze żadnych miesiączek",
+
+  // Mark Modal
+  mark: "oznacz", // eg. mark your period
+  cancel: "anuluj",
+
+  // Welcome Modal
+  "Welcome to Peri": "Witaj w Peri",
+  "Mark the days of your": "Oznacz dni swojej", // eg. Mark the days of your last period
+  "last period": "ostatniej miesiączki", // eg. Mark the days of your last period
+  Continue: "Kontynuuj",
+  "Forecast will not be generated.": "Prognoza nie zostanie wygenerowana",
+  or: "lub",
+
+  // Info Modal
+  "Frequent symptoms": "Częste objawy",
+  "is current phase of cycle": "jest obecną fazą cyklu", // eg. Menstrual phase is current phase of cycle"
+
+  // Phases info
+  "Menstrual phase": "Faza menstruacyjna",
+  "This cycle is accompanied by low hormone levels.":
+    "Temu cyklowi towarzyszą niskie poziomy hormonów.",
+  "lack of energy and strength": "brak siły i energii",
+  pain: "ból",
+  "weakness and irritability": "osłabienie i drażliwość",
+  "increased appetite": "powiększony apetyt",
+
+  "Follicular phase": "Faza folikularna",
+  "The level of estrogen in this phase rises and reaches a maximum level.":
+    "Poziom estrogeniu w tej fazie wzrasta i osiąga maksymalny poziom.",
+  "strength and vigor appear": "pojawiają się siła i wigor",
+  "endurance increases": "wzrasta wytrzymałość",
+  "new ideas and plans appear": "pojawiają sie nowe pomysły i plany",
+  "libido increases": "wzrasta libido",
+
+  "Ovulation phase": "Faza owulacji",
+  "Once estrogen levels peak, they trigger the release of two important ovulation hormones, follicle-stimulating hormone and luteinizing hormone.":
+    "Gdy poziom estrogenu osiąga szczyt, powoduje to uwolnienie dwóch ważnych hormonów owulacyjnych, hormonu folikulotropowego i hormonu luteinizującego.",
+  "increased sexual desire": "zwiększone pragnienie seksualne",
+  "optimistic mood": "optymistyczny nastrój",
+  "mild fever": "łagodna gorączka",
+  "lower abdominal pain": "ból dolnej części brzucha",
+  "chest discomfort and bloating": "dyskomfort w klatce piersiowej i wzdęcia",
+  "characteristic secretions ": "charakterystyczna wydzielina",
+
+  "Luteal phase": "Faza lutealna",
+  "Levels of the hormones estrogen and progesterone first rise and then drop sharply just before a period. Progesterone reaches its peak in the luteal phase.":
+    "Najpierw wzrastają poziomy estrogeniu i progesteronu, a później gwałtownie opadają tuż przed miesiączką. Poziom progesteronu osiąga swój szczyt w fazie lutealnej.",
+  "breast tenderness": "delikatność piersi",
+  puffiness: "opuchlizna",
+  "acne and skin rashes": "trądzik i wysypki skórne",
+  "diarrhea or constipation": "biegunka lub zaparcia",
+  "irritability and depressed mood": "drażliwość i depresyjny nastrój",
+
+  // Menu
+  Preferences: "Preferencje",
+  Edit: "Edytuj",
+  Language: "Język",
+  Theme: "Motyw",
+  "Import config": "Zaimportuj konfigurację",
+  "Export config": "Wyeksportuj konfigurację",
+  "Configuration has been imported": "Konfiguracja została zaimportowana",
+  "Download latest version": "Pobierz najnowszą wersję",
+  "We are on GitHub": "Jesteśmy na GitHubie",
+  "Stored cycles count": "Liczba zapisanych cykli",
+
+  // Alert Demo
+  "This is just a demo": "To jest jedynie demo",
+  "You can download the application ": "Aplikację możesz pobrać ", // eg. You can download the application here
+  here: "tutaj", // eg. You can download the application here
+
+  // Notifications
+  Notifications: "Powiadomienia",
+  "Period is coming soon": "Wkrótce miesiączka",
+  "Your period may start tomorrow": "Twoja miesiączka może się zacząć jutro",
+  "Your period may start today": "Twoja miesiączka może się zacząć dzisiaj",
+
+  //Count stored cycles
+  "Confirm selection": "Potwierdź wybór",
+  "Are you sure you want to change the number of stored cycles?":
+    "Masz pewność, że chcesz zmienić liczbę zapisanych cykli?",
+  "Reducing the number will permanently remove some cycles.":
+    "Zmniejszenie liczby permanentnie usunie niektóre cykle.",
+};
+
+export default pl;
