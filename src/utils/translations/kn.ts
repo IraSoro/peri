b4c53224c4cc2c896deac3e20be2b9c6167726f7
Lakshmi Sowmya .L <sowmyasri4002@gmail.com>
2025-10-28 23:54:12 +0530

    added kananda translation

---
+++ src/utils/translations/kn.ts
@@ -0,0 +1,102 @@
+const kn = {
+  // Home Tab
+  Home: "ಮುಖಪುಟ",
+  "Period in": "ಮಾಸಿಕಿ ಆಗಲು",
+  "no info": "ಮಾಹಿತಿ ಇಲ್ಲ",
+  Period: "ಮಾಸಿಕಿ",
+  today: "ಇಂದು",
+  Delay: "ವಿಳಂಬ",
+  Days:
+    "(0)[ದಿನಗಳು];(1)[ದಿನ];(2-4)[ದಿನಗಳು];(5-20)[ದಿನಗಳು];(21)[ದಿನ];(22-24)[ದಿನಗಳು];(25-30)[ದಿನಗಳು];(31)[ದಿನ];(32-34)[ದಿನಗಳು];(35-40)[ದಿನಗಳು];",
+  "Current cycle day": "ಪ್ರಸ್ತುತ ಚಕ್ರದ ದಿನ",
+  day: "{{count}}ನೇ ದಿನ",
+  Ovulation: "ಒವ್ಯುಲೇಶನ್",
+  possible: "ಸಾಧ್ಯ",
+  finished: "ಮುಗಿದಿದೆ",
+  tomorrow: "ನಾಳೆ",
+  in: "ನಲ್ಲಿ",
+  "chance of getting pregnant": "ಗರ್ಭಧಾರಣೆಯ ಸಾಧ್ಯತೆ",
+  High: "ಹೆಚ್ಚು",
+  Low: "ಕಡಿಮೆ",
+  "Period today": "ಇಂದು ಮಾಸಿಕಿ",
+  edit: "ಸಂಪಾದಿಸು",
+  save: "ಉಳಿಸಿ",
+  "Period is": "ಮಾಸಿಕಿ ಇದೆ",
+  "possible today": "ಇಂದು ಸಾಧ್ಯವಿದೆ",
+
+  // Details Tab
+  Details: "ವಿವರಗಳು",
+  "Period length": "ಮಾಸಿಕಿಯ ಅವಧಿ",
+  "Cycle length": "ಚಕ್ರದ ಉದ್ದ",
+  "You haven't marked any periods yet": "ನೀವು ಇನ್ನೂ ಯಾವುದೇ ಮಾಸಿಕಿಗಳನ್ನು ಗುರುತಿಸಿಲ್ಲ",
+
+  // Mark Modal
+  mark: "ಗುರುತಿಸು",
+  cancel: "ರದ್ದುಮಾಡಿ",
+
+  // Welcome Modal
+  "Welcome to Peri": "Peri ಗೆ ಸ್ವಾಗತ",
+  "Mark the days of your": "ನಿಮ್ಮ ದಿನಗಳನ್ನು ಗುರುತಿಸಿ",
+  "last period": "ಕೊನೆಯ ಮಾಸಿಕಿ",
+  Continue: "ಮುಂದುವರಿಸಿ",
+  "Forecast will not be generated.": "ಭವಿಷ್ಯವಾಣಿ ರಚಿಸಲಾಗುವುದಿಲ್ಲ.",
+  or: "ಅಥವಾ",
+
+  // Info Modal
+  "Frequent symptoms": "ಸಾಮಾನ್ಯ ಲಕ್ಷಣಗಳು",
+  "is current phase of cycle": "- ಚಕ್ರದ ಪ್ರಸ್ತುತ ಹಂತ",
+
+  // Phases info
+  "Menstrual phase": "ಮಾಸಿಕ ಹಂತ",
+  "This cycle is accompanied by low hormone levels.":
+    "ಈ ಚಕ್ರದಲ್ಲಿ ಹಾರ್ಮೋನ್ ಮಟ್ಟಗಳು ಕಡಿಮೆಯಾಗಿರುತ್ತವೆ.",
+  "lack of energy and strength": "ಶಕ್ತಿ ಮತ್ತು ಉತ್ಸಾಹದ ಕೊರತೆ",
+  pain: "ನೋವು",
+  "weakness and irritability": "ದೌರ್ಬಲ್ಯ ಮತ್ತು ಚಿಟ್ಟೆ",
+  "increased appetite": "ಆಹಾರದ ಆಸೆ ಹೆಚ್ಚಲಾಗಿದೆ",
+
+  "Follicular phase": "ಫಾಲಿಕ್ಯುಲರ್ ಹಂತ",
+  "The level of estrogen in this phase rises and reaches a maximum level.":
+    "ಈ ಹಂತದಲ್ಲಿ ಈಸ್ಟ್ರೋಜೆನ್ ಮಟ್ಟವು ಏರುತ್ತು ಮತ್ತು ಗರಿಷ್ಠಕ್ಕೆ ತಲುಪುತ್ತದೆ.",
+  "strength and vigor appear": "ಶಕ್ತಿ ಮತ್ತು ಉತ್ಸಾಹ ವ್ಯಕ್ತವಾಗುತ್ತವೆ",
+  "endurance increases": "ಸಹನೆ ಹೆಚ್ಚುತ್ತದೆ",
+  "new ideas and plans appear": "ಹೊಸ ಆಲೋಚನೆಗಳು ಮತ್ತು ಯೋಜನೆಗಳು ಉಂಟಾಗುತ್ತವೆ",
+  "libido increases": "ಲೈಂಗಿಕ ಆಸೆ ಹೆಚ್ಚುವುದು",
+
+  "Ovulation phase": "ಒವ್ಯುಲೇಶನ್ ಹಂತ",
+  "Once estrogen levels peak, they trigger the release of two important ovulation hormones, follicle-stimulating hormone and luteinizing hormone.":
+    "ಈಸ್ಟ್ರೋಜೆನ್ ಮಟ್ಟಗಳು ಗರಿಷ್ಠಕ್ಕೆ ತಲುಪಿದಾಗ, ಅದು ಎರಡು ಪ್ರಮುಖ ಒವ್ಯುಲೇಶನ್ ಹಾರ್ಮೋನ್‌ಗಳನ್ನ — ಫಾಲಿಕಲ್-ಸ್ಟಿಮ್ಯುಲೇಟಿಂಗ್ ಹಾರ್ಮೋನ್ (FSH) ಮತ್ತು ಲ್ಯೂಟಿನೈಸಿಂಗ್ ಹಾರ್ಮೋನ್ (LH) — ಬಿಡುಗಡೆಗೊಳಿಸುತ್ತದೆ.",
+  "increased sexual desire": "ಲೈಂಗಿಕ ಆಸೆಯು ಹೆಚ್ಚುತ್ತದೆ",
+  "optimistic mood": "ಆಶಾವಾದಿ ಮನೋಭಾವ",
+  "mild fever": "ಸ್ವಲ್ಪ ಜ್ವರ",
+  "lower abdominal pain": "ಕೆಳ ಹೊಟ್ಟೆ ನೋವು",
+  "chest discomfort and bloating": "ಸ್ತನದ ಅಸ್ವಸ್ಥತೆ ಮತ್ತು ಉಬ್ಬರ",
+  "characteristic secretions": "ವಿಶಿಷ್ಟ ವಿಸರ್ಜನೆಗಳು",
+
+  "Luteal phase": "ಲುಟಿಯಲ್ ಹಂತ",
+  "Levels of the hormones estrogen and progesterone first rise and then drop sharply just before a period. Progesterone reaches its peak in the luteal phase.":
+    "ಈಸ್ಟ್ರೋಜೆನ್ ಮತ್ತು ಪ್ರೊಜೆಸ್ಟೆರೋನ್ ಹಾರ್ಮೋನ್‌ಗಳ ಮಟ್ಟಗಳು ಮೊದಲು ಏರುತ್ತವೆ ಮತ್ತು ನಂತರ ಮಾಸಿಕಿಯ ಮೊದಲಿಗೆ ತೀಕ್ಷ್ಣವಾಗಿ ಕುಸಿಯುತ್ತವೆ. ಪ್ರೊಜೆಸ್ಟೆರೋನ್ luteal ಹಂತದಲ್ಲಿ ಗರಿಷ್ಠಕ್ಕೆ ತಲುಪುತ್ತದೆ.",
+  "breast tenderness": "ಸ್ತನದ ಸ್ಪರ್ಶ ಸಂವೇದನೆ",
+  puffiness: "ಶೋಥ",
+  "acne and skin rashes": "ಚರ್ಮದ ಪುಟ್ಟುಗಳು ಮತ್ತು ರ‍್ಯಾಶ್‌ಗಳು",
+  "diarrhea or constipation": "ದಸ್ತು ಅಥವಾ ಕೋಷ್ಟಕಟ್ಟುವಿಕೆ",
+  "irritability and depressed mood": "ಚಿತ್ತಕೆಡ ಮತ್ತು ದುಃಖದ ಮನೋಭಾವ",
+
+  // Menu
+  Preferences: "ಆಯ್ಕೆಗಳು",
+  Edit: "ಸಂಪಾದಿಸು",
+  Language: "ಭಾಷೆ",
+  Theme: "ಥೀಮ್",
+  "Import config": "ಕಾನ್ಫಿಗ್ ಆಮದು ಮಾಡಿ",
+  "Export config": "ಕಾನ್ಫಿಗ್ ರಫ್ತು ಮಾಡಿ",
+  "Configuration has been imported": "ಕಾನ್ಫಿಗರೇಶನ್ ಆಮದು ಮಾಡಲಾಗಿದೆ",
+  "Download latest version": "ಇತ್ತೀಚಿನ ಆವೃತ್ತಿಯನ್ನು ಡೌನ್ಲೋಡ್ ಮಾಡಿ",
+
+  // Alert Demo
+  "This is just a demo": "ಇದು ಕೇವಲ ಡೆಮೊ",
+  "You can download the application ": "ನೀವು ಅಪ್ಲಿಕೇಶನ್ ಅನ್ನು ಡೌನ್ಲೋಡ್ ಮಾಡಬಹುದು ",
+  here: "ಇಲ್ಲಿ",
+  "We are on GitHub": "ನಾವು GitHub ನಲ್ಲಿ ಇದ್ದೇವೆ",
+};
+
+export default kn;
