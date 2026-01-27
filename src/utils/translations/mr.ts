82902b1c3832d9e4c4b5fd974b7111108a4327aa
MadhuriRathod30 <madhurirathod.work@gmail.com>
2026-01-27 09:59:00 +0530

    Fixed the formatting

--- src/utils/translations/mr.ts
+++ src/utils/translations/mr.ts
@@ -1,41 +1,39 @@
 const mr = {
   Home: "मुख पृष्ठ",
-  "Period in": "पाळी येण्यास", 
+  "Period in": "पाळी येण्यास",
   "no info": "माहिती उपलब्ध नाही",
-  Period: "पाळी", 
+  Period: "पाळी",
   today: "आज",
-  Delay: "पाळी उशीर", 
-  Days: "दिवस", 
+  Delay: "पाळी उशीर",
+  Days: "दिवस",
   "Current cycle day": "सध्याचा सायकल दिवस",
 
-  day: "पाळी – {{count}}वा दिवस", 
+  day: "पाळी – {{count}}वा दिवस",
   Ovulation: "अंडोत्सर्जन",
-  possible: "संभाव्य", 
-  finished: "पूर्ण झाले", 
-  tomorrow: "उद्या", 
-  in: "मध्ये", 
-  "chance of getting pregnant": "गर्भधारणेची शक्यता", 
+  possible: "संभाव्य",
+  finished: "पूर्ण झाले",
+  tomorrow: "उद्या",
+  in: "मध्ये",
+  "chance of getting pregnant": "गर्भधारणेची शक्यता",
   High: "जास्त",
   Low: "कमी",
   "Period today": "आज पाळी",
   edit: "संपादन",
   save: "जतन करा",
-  "Period is": "पाळी आहे", 
+  "Period is": "पाळी आहे",
   "possible today": "आज शक्यता आहे",
 
   Details: "तपशील",
-  "Period length": "पाळीचा कालावधी", 
-  "Cycle length": "सायकलचा कालावधी", 
+  "Period length": "पाळीचा कालावधी",
+  "Cycle length": "सायकलचा कालावधी",
   "You haven't marked any periods yet": "तुम्ही अजून पाळी नोंदवलेली नाही",
 
-  
-  mark: "नोंद करा", 
+  mark: "नोंद करा",
   cancel: "रद्द करा",
 
-  
   "Welcome to Peri": "पेरी मध्ये तुमचे स्वागत आहे",
-  "Mark the days of your": "तुमच्या पाळीचे दिवस नोंद करा", 
-  "last period": "शेवटची पाळी", 
+  "Mark the days of your": "तुमच्या पाळीचे दिवस नोंद करा",
+  "last period": "शेवटची पाळी",
   Continue: "पुढे चला",
   "Forecast will not be generated.": "अंदाज तयार केला जाणार नाही.",
   or: "किंवा",
@@ -44,14 +42,16 @@ const mr = {
   "is current phase of cycle": "हा सायकलचा सध्याचा टप्पा आहे", // eg. Menstrual phase is current phase of cycle"
 
   "Menstrual phase": "मासिक पाळीचा टप्पा",
-  "This cycle is accompanied by low hormone levels.": "या टप्प्यात हार्मोन्सची पातळी कमी असते.",
+  "This cycle is accompanied by low hormone levels.":
+    "या टप्प्यात हार्मोन्सची पातळी कमी असते.",
   "lack of energy and strength": "ऊर्जा आणि ताकद कमी वाटणे",
   pain: "वेदना",
   "weakness and irritability": "अशक्तपणा आणि चिडचिड",
   "increased appetite": "भूक वाढणे",
 
   "Follicular phase": "फॉलिक्युलर टप्पा",
-  "The level of estrogen in this phase rises and reaches a maximum level.": "या टप्प्यात इस्ट्रोजेन हार्मोनची पातळी वाढून कमाल स्तरावर पोहोचते.",
+  "The level of estrogen in this phase rises and reaches a maximum level.":
+    "या टप्प्यात इस्ट्रोजेन हार्मोनची पातळी वाढून कमाल स्तरावर पोहोचते.",
   "strength and vigor appear": "ताकद आणि उत्साह जाणवतो",
   "endurance increases": "सहनशक्ती वाढते",
   "new ideas and plans appear": "नवीन कल्पना आणि योजना सुचतात",
@@ -76,31 +76,31 @@ const mr = {
   "diarrhea or constipation": "जुलाब किंवा बद्धकोष्ठता",
   "irritability and depressed mood": "चिडचिड आणि उदास मनःस्थिती",
 
-Preferences: "प्राधान्ये",
-Edit: "संपादन",
-Language: "भाषा",
-Theme: "थीम",
-"Import config": "सेटिंग आयात करा",
-"Export config": "सेटिंग निर्यात करा",
-"Configuration has been imported": "सेटिंग यशस्वीरित्या आयात झाली",
-"Download latest version": "नवीनतम आवृत्ती डाउनलोड करा",
-"We are on GitHub": "आम्ही GitHub वर आहोत",
-"Stored cycles count": "जतन केलेल्या सायकल्सची संख्या",
+  Preferences: "प्राधान्ये",
+  Edit: "संपादन",
+  Language: "भाषा",
+  Theme: "थीम",
+  "Import config": "सेटिंग आयात करा",
+  "Export config": "सेटिंग निर्यात करा",
+  "Configuration has been imported": "सेटिंग यशस्वीरित्या आयात झाली",
+  "Download latest version": "नवीनतम आवृत्ती डाउनलोड करा",
+  "We are on GitHub": "आम्ही GitHub वर आहोत",
+  "Stored cycles count": "जतन केलेल्या सायकल्सची संख्या",
 
-"This is just a demo": "हे फक्त डेमो आहे",
-"You can download the application ": "तुम्ही अ‍ॅप डाउनलोड करू शकता ",
-here: "इथे",
+  "This is just a demo": "हे फक्त डेमो आहे",
+  "You can download the application ": "तुम्ही अ‍ॅप डाउनलोड करू शकता ",
+  here: "इथे",
 
-Notifications: "सूचना",
-"Period is coming soon": "पाळी लवकरच येणार आहे",
-"Your period may start tomorrow": "तुमची पाळी उद्या येऊ शकते",
-"Your period may start today": "तुमची पाळी आज येऊ शकते",
+  Notifications: "सूचना",
+  "Period is coming soon": "पाळी लवकरच येणार आहे",
+  "Your period may start tomorrow": "तुमची पाळी उद्या येऊ शकते",
+  "Your period may start today": "तुमची पाळी आज येऊ शकते",
 
-"Confirm selection": "निवड निश्चित करा",
-"Are you sure you want to change the number of stored cycles?":
-  "जतन केलेल्या सायकल्सची संख्या बदलायची आहे का?",
-"Reducing the number will permanently remove some cycles.":
-  "संख्या कमी केल्यास काही सायकल्स कायमस्वरूपी हटवल्या जातील.",
+  "Confirm selection": "निवड निश्चित करा",
+  "Are you sure you want to change the number of stored cycles?":
+    "जतन केलेल्या सायकल्सची संख्या बदलायची आहे का?",
+  "Reducing the number will permanently remove some cycles.":
+    "संख्या कमी केल्यास काही सायकल्स कायमस्वरूपी हटवल्या जातील.",
 };
 
 export default mr;
